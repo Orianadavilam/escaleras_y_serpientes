fe94b2c0bb741953398b57aba0962012dae0ccf6
Ori <orianadavilam@gmail.com>
2021-10-17 19:30:06 -0300

    [V1-KISS] FuncionesCrearTablero&ImprimirTablero
    Funciona con los datos de un solo jugador.. Y sin tweaks

--- TP01.py
+++ TP01.py
@@ -1,6 +1,53 @@
 import random 
 import time
 
+def crearTablero(cantidad_filas:int, cantidad_columnas:int) -> list:
+    #Crea un tablero con la cantidad de filas y el numero de casilleros de cada una
+    #Retorna una lista de filas de tamaño "cantidad_columnas"
+    ultimo_casillero_fila_anterior:int = 1 
+    tablero:list = []
+
+    for fila in range(1, cantidad_filas+1):
+        casilleros_fila:list = []
+
+        for casillero in range(ultimo_casillero_fila_anterior, ultimo_casillero_fila_anterior + cantidad_columnas):
+            casilleros_fila.append(casillero)
+        
+        ultimo_casillero_fila_anterior = casillero + 1
+        tablero.append(casilleros_fila)
+
+    return tablero
+
+def imprimirTablero(tablero:list, posiciones_especiales:dict, jugadores:list) -> None:
+    #Necesito que imprima las filas al reves 
+    tablero.sort(reverse=True)
+
+    for fila in range(len(tablero)):
+        casilleros_fila:list =  tablero[fila] 
+        if (fila % 2 == 0):
+            #Si la fila es par, ordeno de manera descendente
+            casilleros_fila.sort(reverse=True)
+        
+        for casillero in casilleros_fila:
+            if (casillero in posiciones_especiales.keys()):
+                if ( casillero < posiciones_especiales.get(casillero, 0)):
+                    #Es una escalera 
+                    print(f"| {casillero} (E)", end=' ')
+                elif (casillero > posiciones_especiales.get(casillero)): 
+                    #Es una serpiente (ponele)
+                    print(f"| {casillero} (S)", end=' ')                    
+            else:    
+                print(f"| {casillero}", end=' ')
+
+            for jugador in jugadores:
+                datos_jugador:dict = jugador
+
+                if (casillero in datos_jugador.values()):
+                    for nombre in datos_jugador.keys():
+                        print(f" ({nombre})", end = '')
+
+        print("")
+
 def main() -> None: 
     #Constantes
     CANTIDAD_OPCIONES_MENU:int = 4
@@ -11,7 +58,7 @@ def main() -> None:
     CANTIDAD_FILAS_TABLERO:int = 10
     CANTIDAD_COLUMNAS_TABLERO:int = 10
 
-    #Constante del diccionario con las posiciones de las escaleras y serpientes 
+    #Constante del diccionario con las casilleroes de las escaleras y serpientes 
     POS_ESCALERAS_SERPIENTES:dict = {3: 18, 6: 67, 57: 83, 72: 89, 85: 96, 86: 45, 88: 31, 98: 79, 63: 22, 58: 37, 48: 12, 36: 17}
 
     #Realizo la creacion del menú 
@@ -34,73 +81,43 @@ def main() -> None:
         hay_ganador:bool = False
 
         #Obvio falta validar aqui el tema de que sean solo letras
-        nombre_jugador_1:str = input("Ingrese el nombre del jugador 1: ")
-        nombre_jugador_2:str = input("Ingrese el nombre del jugador 2: ")
+        nombre_jugador_1:str = "Ori" #input("Ingrese el nombre del jugador 1: ")
+        nombre_jugador_2:str = "Jesus" #input("Ingrese el nombre del jugador 2: ")
         jugador_1:dict = {nombre_jugador_1 : 0} #Nombre : valor_dados
         jugador_2:dict = {nombre_jugador_2 : 0} #Nombre : valor_dados
+        jugadores:list = [jugador_1, jugador_2]
+        tablero:list = crearTablero(CANTIDAD_FILAS_TABLERO, CANTIDAD_COLUMNAS_TABLERO)
 
         while not hay_ganador:
-            input(f"{nombre_jugador_1} por favor presiona una tecla para lanzar los dados")
+            #input(f"{nombre_jugador_1} por favor presiona una tecla para lanzar los dados")
             valor_dados = random.randint(1, 6)
             print(f"{nombre_jugador_1} sacaste un: {valor_dados}")
             time.sleep(1)
 
             jugador_1 [nombre_jugador_1] += valor_dados
-            posicion_a_mover = jugador_1.get(nombre_jugador_1)   
+            casillero_a_mover = jugador_1.get(nombre_jugador_1)   
 
-            if (posicion_a_mover in POS_ESCALERAS_SERPIENTES):
+            if (casillero_a_mover in POS_ESCALERAS_SERPIENTES):
                 accion:str = "avanzas"
                 nombre_casilla = "escalera"
 
-                posicion_nueva = POS_ESCALERAS_SERPIENTES.get(posicion_a_mover, 0)
+                casillero_nueva = POS_ESCALERAS_SERPIENTES.get(casillero_a_mover, 0)
 
-                if(posicion_a_mover > posicion_nueva):
+                if(casillero_a_mover > casillero_nueva):
                     accion = "retrocedes"
                     nombre_casilla = "serpiente"
 
-                posicion_a_mover = posicion_nueva    
-                print(f"{nombre_jugador_1} caíste en una {nombre_casilla}, así que {accion} hasta la posicion: {posicion_a_mover}")
+                casillero_a_mover = casillero_nueva    
+                print(f"{nombre_jugador_1} caíste en una {nombre_casilla}, así que {accion} hasta la casillero: {casillero_a_mover}")
             else:
-                print(f"{nombre_jugador_1} avanzas hasta la posicion: {posicion_a_mover}")
+                print(f"{nombre_jugador_1} avanzas hasta la casillero: {casillero_a_mover}")
             
             time.sleep(1)
-            jugador_1 [nombre_jugador_1] = posicion_a_mover
+            jugador_1 [nombre_jugador_1] = casillero_a_mover
             hay_ganador = jugador_1.get(nombre_jugador_1) >= CANTIDAD_FILAS_TABLERO * CANTIDAD_COLUMNAS_TABLERO
 
-            ultima_pos_fila:int = CANTIDAD_FILAS_TABLERO * CANTIDAD_COLUMNAS_TABLERO + 1
-        
-            for fila in range(1, CANTIDAD_FILAS_TABLERO + 1):
-                posicion_desde:int = ultima_pos_fila - 1
-                posicion_nueva:int = ultima_pos_fila - CANTIDAD_COLUMNAS_TABLERO - 1
-                step:int = -1
-
-                if (fila % 2 == 0):
-                    #Si la fila es par, voy en orden descendente
-                    posicion_desde = ultima_pos_fila - CANTIDAD_COLUMNAS_TABLERO
-                    posicion_nueva = ultima_pos_fila 
-                    step = 1
-
-                for posicion in range(posicion_desde, posicion_nueva, step):
-                    if (posicion in POS_ESCALERAS_SERPIENTES.keys() or posicion == jugador_1.get(nombre_jugador_1)):
-                        if ( posicion in POS_ESCALERAS_SERPIENTES.keys() and posicion < POS_ESCALERAS_SERPIENTES.get(posicion, 0)):
-                            #Es una escalera 
-                            print(f"| {posicion} (E)", end=' ')
-                        elif (posicion in POS_ESCALERAS_SERPIENTES.keys() and posicion > POS_ESCALERAS_SERPIENTES.get(posicion)): 
-                            #Es una serpiente (ponele)
-                            print(f"| {posicion} (S)", end=' ')                    
-                    else:    
-                        print(f"| {posicion}", end=' ')
-
-                    if(posicion == jugador_1.get(nombre_jugador_1)):
-                        print(f"| {posicion} ({nombre_jugador_1})", end=' ')
-
-                print("|")
-                if (fila % 2 == 0):
-                    ultima_pos_fila = posicion_desde
-                else:
-                    ultima_pos_fila = posicion
-
-                print("")
+            imprimirTablero(tablero, POS_ESCALERAS_SERPIENTES, jugadores)
+            
     elif (opcion == OP_MOSTRAR_ESTADISTICAS):
         print(" ----- ESTADISTICAS DEL JUEGO ----- ")
     elif (opcion == OP_RESET_ESTADISTICAS):
